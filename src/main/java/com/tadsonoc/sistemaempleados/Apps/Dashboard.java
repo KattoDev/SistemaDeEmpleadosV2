21f4c4898c3f1e40544ee36e9fcb62d9e4258ea6
Ritzz <alexisrincon27@outlook.com>
2024-11-20 14:24:00 -0500

    add Dashboard

--- src/main/java/com/tadsonoc/sistemaempleados/Apps/Dashboard.java
+++ src/main/java/com/tadsonoc/sistemaempleados/Apps/Dashboard.java
@@ -1,12 +1,28 @@
 package com.tadsonoc.sistemaempleados.Apps;
 
+import com.tadsonoc.sistemaempleados.Models.WindowSize;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Dashboard extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                Dashboard.class.getResource(
+                        "/com/tadsonoc/sistemaempleados/Views/dashboard-view.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                WindowSize.getDashboard()[0],
+                WindowSize.getDashboard()[1]);
+        stage.setTitle("Sistema De Empleados - Dashboard");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
     }
 }
