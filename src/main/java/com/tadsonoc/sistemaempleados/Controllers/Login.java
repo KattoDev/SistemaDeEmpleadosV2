0e5c366fa2d5d8b01c6fd2bb7b4ad522be892210
Ritzz <alexisrincon27@outlook.com>
2024-11-21 11:52:56 -0500

    Add Login Auth

--- src/main/java/com/tadsonoc/sistemaempleados/Controllers/Login.java
+++ src/main/java/com/tadsonoc/sistemaempleados/Controllers/Login.java
@@ -1,10 +1,17 @@
 package com.tadsonoc.sistemaempleados.Controllers;
 
+import com.tadsonoc.sistemaempleados.Apps.Dashboard;
 import com.tadsonoc.sistemaempleados.Models.User;
+import com.tadsonoc.sistemaempleados.Models.WindowSize;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Login {
     @FXML
@@ -13,12 +20,10 @@ public class Login {
     @FXML
     private PasswordField password_field;
 
-    @FXML
-    protected void LoginButtonClick() {
-
+    public void LoginButtonClick(MouseEvent mevt) throws IOException, URISyntaxException {
         if (!user_field.getText().isEmpty() && !password_field.getText().isEmpty()) {
 
-            User user = new User(user_field.getText(), user_field.getText());
+            User user = new User(user_field.getText(), password_field.getText());
 
             if (!user.Login()) {
                 Alerts.showAlert(
@@ -26,7 +31,16 @@ public class Login {
                         null,
                         "Usuario o contraseña incorrectos");
             } else {
-                Alerts.showAlert(Alert.AlertType.INFORMATION, null, "auth");
+                Stage stage = (Stage) ((javafx.scene.Node) mevt.getSource()).getScene().getWindow();
+                stage.close();
+                // Lanzar el Dashboard
+                try {
+                    Dashboard dashboard = new Dashboard();
+                    Stage dashboardStage = new Stage();
+                    dashboard.start(dashboardStage);
+                } catch (NullPointerException e) {
+                    e.getCause();
+                }
             }
 
         } else {
