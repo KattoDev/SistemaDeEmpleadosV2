6cab4e27fef11578d518ea642c787df43c6f441e
Ritzz <alexisrincon27@outlook.com>
2024-11-21 11:58:22 -0500

    Modify WindowSize controller

--- src/main/java/com/tadsonoc/sistemaempleados/Models/WindowSize.java
+++ src/main/java/com/tadsonoc/sistemaempleados/Models/WindowSize.java
@@ -2,13 +2,13 @@ package com.tadsonoc.sistemaempleados.Models;
 
 public class WindowSize {
     private static final int[] login = {640, 480};
-    private static final int[] dashboard = {1280, 800};
+    private static final int[] dashboard = {1280, 700};
 
     public static int[] getLogin() {
         return login;
     }
 
-    public static  int[] getDashboard() {
+    public static int[] getDashboard() {
         return dashboard;
     }
 }
